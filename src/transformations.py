b2ac6103553d976808a6a67e6665c817494835b5
THinnerichs <tilman@hinnerichs.com>
2020-03-24 19:32:05 +0100

    Fixing transformation datatype issue 5

--- src/transformations.py
+++ src/transformations.py
@@ -531,7 +531,7 @@ def greyscale_sinkhorn_ball_perturbation(X,
     X_best = X.clone()
     result = net(normalize(X))
     print(result.size())
-    err_best = err = net(normalize(X)).max(1)[1] != y
+    err_best = err = net(normalize(X)).max(1) != y
     epsilon_best = epsilon.clone()
 
     t = 0
